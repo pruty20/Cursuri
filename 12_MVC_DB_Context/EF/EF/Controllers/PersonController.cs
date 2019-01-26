436a269fcc1c1b10ade0742d0446c13c8039b344
pruty20 <rpruteanu@gmail.com>
2019-01-26 03:43:16 -0800

    26Jan13:43_Curs12_MVC_DB_Context

---
+++ 12_MVC_DB_Context/EF/EF/Controllers/PersonController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EF.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EF.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly PersonContext context;
+
+        public PersonController(PersonContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var persons = context.Persons.ToList();
+
+            return View(persons);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(model);
+
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+
+        // Get: /Cats/Edit?id
+        [HttpGet]  // Atribut
+        public IActionResult Edit(int id)
+        {
+            Person personToEdit = context.Persons.Find(id);
+              
+            return View(personToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                var myPerson = context.Persons.Find(model.PersonId);
+                TryUpdateModelAsync(myPerson);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+
+
+    }
+}
---
+++ 12_MVC_DB_Context/EF/EF/Data/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF.Data
+{
+    public class Person // going to be an entity
+    {
+        public int PersonId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string  LastName { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
